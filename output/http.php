30f1e5abdba7ab403b12f5659d889f0aaa50c18f
Alex Smith <asmith@curse.com>
2012-08-08 15:44:33 -0500

    Super basic HTML output module.

---
+++ output/http.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * NoName Studios
+ * Mouse Framework
+ * Mouse Output HTTP - Screen output to a HTTP based interface.
+ *
+ * @author 		Alexia E. Smith
+ * @copyright	(c) 2010 - 2012 NoName Studios
+ * @license		All Rights Reserved
+ * @package		Mouse Framework
+ * @link		http://www.nonamestudios.com/
+ * @version		2.0
+ *
+**/
+
+class mouseOutputCli {
+	/**
+	 * Output HTML
+	 *
+	 * @var		string
+	 */
+	private $content;
+
+	/**
+	 * Template Folder Location
+	 *
+	 * @var		string
+	 */
+	protected $templateFolder;
+
+	/**
+	 * Constructor
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	public function __construct($mouse) {
+		$this->config	=& mouseHole::$config;
+	}
+
+	/**
+	 * Template Loader
+	 *
+	 * @access	public
+	 * @param	string	Template name after 'skin_'.  To load skin_articles, pass in articles.
+	 * @return	boolean	Success
+	 */
+	public function loadTemplate($skin) {
+		if (!$skin) {
+			return false;
+		} else {
+			$skinClass = "skin_{$skin}";
+		}
+
+		$file = "{$this->templateFolder}/{$skinClass}.php";
+		var_dump($file);
+		if (is_file($file)) {
+			try {
+				include_once($file);
+				$this->$skin = new $skinClass;
+				return true;
+			} catch (Exception $e) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Template Loader
+	 *
+	 * @access	public
+	 * @param	string	Complete folder path to where skins are stored for this object instance.
+	 * @return	boolean	Success
+	 */
+	public function setTemplateFolder($folder) {
+		if (!$folder) {
+			return false;
+		}
+
+		$folder = rtrim($folder, '/');
+		if (is_dir($folder)) {
+			$this->templateFolder = $folder;
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Add Output
+	 *
+	 * @access	public
+	 * @param	string	String based output
+	 * @return	boolean	Success
+	 */
+	public function addOutput($output) {
+		$this->content .= ($output);
+	}
+
+	/**
+	 * Get Output
+	 *
+	 * @access	public
+	 * @return	string	HTML Content
+	 */
+	public function getOutput() {
+		return $this->content;
+	}
+
+	/**
+	 * Send Output
+	 *
+	 * @access	public
+	 * @return	string	HTML Content
+	 */
+	public function sendOutput() {
+		echo $this->content;
+	}
+
+	/**
+	 * Generates page numbers.
+	 *
+	 * @access	public
+	 * @param	integer	Total number of items to be paginated.
+	 * @param	integer	How many items to display per page.
+	 * @param	integer	Start Position
+	 * @param	integer	Number of extra page numbers to show.
+	 * @return	array	Generated array of pagination information.
+	 */
+	public function generatePagination($totalItems, $itemsPerPage = 100, $start = 0, $extraPages = 4) {
+		if ($totalItems < 1) {
+			return false;
+		}
+
+		$currentPage	= floor($start / $itemsPerPage) + 1;
+		$totalPages		= ceil($totalItems / $itemsPerPage);
+		$lastStart		= floor($totalItems / $itemsPerPage) * $itemsPerPage - $itemsPerPage;
+
+		$pagination['first']	= array('st' => 0, 'selected' => false);
+		$pagination['last']	= array('st' => $lastStart, 'selected' => false);
+		$pagination['stats']	= array('pages' => $totalPages, 'total' => $totalItems, 'current_page' => $currentPage);
+
+		$start	= min($currentPage, $currentPage - ($extraPages / 2));
+		$end	= min($totalPages, $currentPage + ($extraPages / 2));
+
+		if ($start <= 1) {
+			$start = 1;
+			$end = $start + $extraPages;
+		}
+		if ($end >= $totalPages) {
+			$end = $totalPages;
+			$start = max($end - $extraPages, ($currentPage - ($extraPages / 2)) - (($extraPages / 2) - ($end - $currentPage)));
+		}
+
+		for ($i = $start; $i <= $end; $i++) {
+			$pagination['pages'][$i] = array('st' => ($i * $itemsPerPage) - $itemsPerPage, 'selected' => ($i == $currentPage ? true : false));
+		}
+
+		return $pagination;
+	}
+
+	/**
+	 * Generates pagination template.
+	 *
+	 * @access	public
+	 * @param	array	Array of pagination information.
+	 */
+	public function paginationTemplate($pagination) {
+		if (count($pagination['pages'])) {
+$HTML .= <<<HTML
+	<ul class='pagination'>
+HTML;
+			if ($pagination['stats']) {
+				$HTML .= "<li class='pagination_stats'>Page {$pagination['stats']['current_page']} of {$pagination['stats']['pages']}</li>";
+			}
+			if ($pagination['first']) {
+				$HTML .= "<li><a href='?st={$pagination['first']['st']}'>&laquo;</a></li>";
+			}
+			foreach ($pagination['pages'] as $page => $info) {
+				if ($page > 0) {
+					$HTML .= "<li".($info['selected'] ? " class='selected'" : null)."><a href='?st={$info['st']}'>{$page}</a></li>";
+				}
+			}
+			if ($pagination['last']) {
+				$HTML .= "<li><a href='?st={$pagination['last']['st']}'>&raquo;</a></li>";
+			}
+$HTML .= <<<HTML
+	</ul>
+HTML;
+		return $HTML;
+		}
+	}
+}
+?>
